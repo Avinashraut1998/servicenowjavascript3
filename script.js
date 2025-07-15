81e23769230c9d2013f76e692beeaeb11a801d87
AvinashRaut1998 <avinashraut2498@gmail.com>
2025-07-15 08:24:15 +0530

    variables

---
+++ script.js
@@ -0,0 +1,12 @@
+
+// Notes for js
+
+
+// console.log("Hello World....!");   
+
+// alert("Hello World...!!!!");
+
+// confirm("Would you like to join javscript ?"); 
+
+
+    
---
+++ variables.js
@@ -0,0 +1,72 @@
+// Variables are Containers for Storing Data.
+
+// x = 100;
+
+// console.log(x);
+
+// var let const
+
+// var studentName = "Tony Stark";
+
+// let schoolName = "XYZSchool";
+
+// const className = 10;
+
+
+
+// console.log(studentName);
+// console.log(schoolName);
+// console.log(className);
+
+
+// JavaScript Identifiers
+// All JavaScript variables must be identified with unique names.
+
+// These unique names are called identifiers.
+
+// Identifiers can be short names (like x and y) or more descriptive names (age, sum, totalVolume).
+
+// The general rules for constructing names for variables (unique identifiers) are:
+
+// Names can contain letters, digits, underscores, and dollar signs.
+// Names must begin with a letter.
+// Names can also begin with $ and _ (but we will not use it in this tutorial).
+// Names are case sensitive (y and Y are different variables).
+// Reserved words (like JavaScript keywords) cannot be used as names.
+
+// var 
+
+
+// var num = 100;
+
+// var num = 200;
+
+// console.log(num);
+
+
+
+// let 
+// let num = 100;
+
+
+// num = 200;  // automatically 
+
+// console.log(num);
+
+
+// const
+
+// const num = 100;
+
+// num = 100;    // automatically 
+
+// console.log(num);
+
+// {
+//     var x= 100;
+//     console.log(x);
+    
+// }
+
+// console.log(x);
+
