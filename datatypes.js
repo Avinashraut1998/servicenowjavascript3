d6f5eacc7b349195860ea122d68029df18ac88dd
AvinashRaut1998 <avinashraut2498@gmail.com>
2025-07-17 07:53:29 +0530

    Strings

---
+++ datatypes.js
@@ -0,0 +1,93 @@
+//Data Types
+
+// string
+
+// var studentName = "abcd123456!@#$%%";        // string
+
+
+
+
+// var studentName = "Tony";        // string
+
+
+// var studentName = 'tONY';        // string
+
+
+// console.log(studentName);
+
+
+// Number
+
+// let age = 2.12547;
+
+// console.log(age);
+
+
+// console.log(typeof age);
+
+
+
+// JavaScript Booleans
+// Booleans can only have two values: true or false.
+
+// let isActive = true;   // boolean value
+
+
+// let isActive = true;   // boolean value
+
+
+// console.log(isActive);
+// console.log(typeof isActive);
+
+
+
+// undefined
+// In JavaScript, a variable without a value, has the value undefined. The type is also undefined.
+
+
+// let carName;          // undefined 
+
+// console.log(carName);
+// console.log(typeof carName);             // undefined 
+
+
+// null
+
+// let time = null;
+
+// console.log(time);
+
+
+// console.log(typeof time);
+
+
+// BigInt
+
+
+// let x = BigInt("123456789012345678901234567890");
+
+
+// console.log(x);
+
+
+// symbol
+
+// const symbol1 = Symbol('description');
+// const symbol2 = Symbol('description');
+
+// console.log(symbol1);
+// console.log(symbol2);
+
+// let res = symbol1 == symbol2;    // type check  value as well as type
+// console.log(res);
+
+
+
+// non primitive datatypes
+
+
+// arrays
+// Object
+// function
+
+   
---
+++ strings.js
@@ -0,0 +1,102 @@
+// JavaScript Strings
+// A string (or a text string) is a series of characters like "John Doe".
+
+// Strings are written with quotes. You can use single or double quotes:
+
+// Strings are Immutable In Javascript.
+
+// let studentName = "Arrya";
+
+
+// trim()
+
+
+// let str = "         test      ";
+
+// console.log(str);
+
+
+// let result = str.trim()
+
+// console.log(str);
+
+// console.log(result);
+
+
+
+//toUpperCase
+
+
+
+// let stuName = "tonystark";
+
+// let result = stuName.toUpperCase();
+
+// console.log(stuName);
+// console.log(result);
+
+
+
+// toLowerCase
+
+// let str = "TONY STARK"
+
+// let result = str.toLowerCase();
+
+// console.log(str);
+// console.log(result);
+
+
+// lenght
+
+// let str = "I Love Coding";
+
+// console.log(str.length);
+
+
+// indexOf   // returns index no     0,1,2,3,4,5,6,7,8,9
+
+
+// let str = "I Love Coding";
+
+// console.log(str);
+
+// let result = str.indexOf('L');
+// console.log(result);
+
+//method chaining
+
+// let msg = "        hello world   ";
+
+// let res = msg.trim().toUpperCase()
+
+// console.log(res);
+
+
+// slice()
+
+// let str = "I Love Coding";
+
+// let res = str.slice(2,6);
+// console.log(res);
+
+//replace()
+
+// let str = "I Love Coding";
+
+// let res = str.replace('Love', 'Like');
+
+// console.log(res);
+
+
+//repeat()
+
+// let str = "I Love Coding";
+// let res = str.repeat(2)
+
+// console.log(res);
+
+
+
+
+
