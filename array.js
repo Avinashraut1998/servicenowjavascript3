7b1b539d9aa2970f31ef51bb375ff0e6bd8b5975
AvinashRaut1998 <avinashraut2498@gmail.com>
2025-07-18 07:55:35 +0530

    Arrays

---
+++ array.js
@@ -0,0 +1,126 @@
+// An array holds more than one value
+// collection of items is known as array.
+
+
+// let arr = ["One", "Two", "Three", "Four", "Five"];
+
+// console.log(arr);
+
+// lenght
+// let arr = ["One", "Two", "Three", "Four", "Five"];
+
+// let res = arr.length
+// console.log(res);
+
+
+// let arr = ["One", "Two", "Three", "Four", "Five"];
+
+// // console.log(arr);
+
+// // console.log(arr[2]);
+
+// //  arr[3] = "Update";          
+//  console.log(arr);
+
+
+// push()
+
+// let fruits = ["mango", "banana", "kiwi", "chikoo", "grapes"]
+
+// fruits.push("watermelon");  // push method returns new lenght of an array
+
+
+// console.log(fruits);
+
+
+//pop()
+
+// let fruits = ["mango", "banana", "kiwi", "chikoo", "grapes", "blueberries"]
+
+
+// fruits.pop();
+
+// console.log(fruits);
+
+// fruits.pop();
+
+// console.log(fruits);
+// fruits.pop();
+
+// console.log(fruits);
+// fruits.pop();
+
+// console.log(fruits);
+// fruits.pop();
+
+// console.log(fruits);
+// fruits.pop();
+
+// console.log(fruits);
+
+
+
+
+// toString();
+
+// let fruits = ["mango", "banana", "kiwi", "chikoo", "grapes", "blueberries"];
+
+// console.log(fruits);
+
+// let res = fruits.toString()
+// console.log(res);
+
+
+//concat()
+
+// let fruits = ["mango", "banana", "kiwi"];
+
+// let fruits2 = [ "chikoo", "grapes", "blueberries"];
+
+// console.log(fruits);
+// console.log(fruits2);
+
+// let res = fruits.concat(fruits2);
+
+// console.log(res);
+
+
+// unshift()
+
+//  let fruits2 = [ "chikoo", "grapes", "blueberries"];
+
+//  let res = fruits2.unshift("mango");
+
+
+//  console.log(fruits2);
+ 
+
+//shift()
+// let fruits = ["mango", "banana", "kiwi", "chikoo", "grapes", "blueberries"];
+
+// fruits.shift();
+// console.log(fruits);
+
+
+
+
+// let fruits = ["mango", "banana", "kiwi", "chikoo", "grapes", "blueberries"];
+
+
+
+
+// let arr = [1,2,3,4,5,true, "one"]
+
+// console.log(arr);
+
+// let arr = [1,2,3,4,5,6,7,[8,9,10,11,[12,13,14,[15,16,17],18,19]]];
+
+
+// console.log(arr[7][4][5])  
+
+
+
+
+
+
+
