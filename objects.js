78e69163bdfd89ae645003e510a85fc2876b6c5d
AvinashRaut1998 <avinashraut2498@gmail.com>
2025-07-21 07:52:30 +0530

    Objects

---
+++ objects.js
@@ -0,0 +1,73 @@
+// Object
+
+
+// let studentInfo = {};
+
+// console.log(studentInfo);
+
+
+// let studentInfo = {
+//     key : value,
+//     key : value,
+//     key : value
+// }
+
+
+
+// let studentInfo = {
+//     Fname : "Tony",
+//     Lname : "Stark",
+//     rollNo : 1010,
+//     isPass : true,
+//     grade : "B"
+// }
+
+// console.log(studentInfo);
+
+
+// let studentInfo = {
+//     Fname : "Tony",
+//     Lname : "Stark",
+//     rollNo : 1010,
+//     isPass : true,
+//     grade : "B"
+// }
+
+
+// let res = studentInfo.grade;
+
+// console.log(res);
+
+
+
+// let student = {
+//     fname : "Arya",
+//     lname : "Stark",
+//     className : 10,
+//     rollNo : 1010,
+//     grade : "A+",
+//     marksObt : "450/500"
+// }
+
+// student.className = 12;
+// student.rollNo = 1020;
+// student.grade = "A++";
+
+// console.log(student);
+
+
+
+
+// let student = {
+//     fname : "Arya",
+//     lname : "Stark",
+//     className : 10,
+//     rollNo : 1010,
+//     grade : "A+",
+//     marksObt : "450/500"
+// }
+
+// let {fname, lname, className} = student;
+
+// console.log(lname);
+
---
+++ operators.js
@@ -0,0 +1,106 @@
+// Operator	Description
+// +	Addition
+// -	Subtraction
+// *	Multiplication
+// **	Exponentiation (ES2016)
+// /	Division
+// %	Modulus (Remainder)
+// ++	Increment
+// --	Decrement
+
+
+
+
+// +	Addition
+
+
+// let a = 15;
+// let b = 20;
+
+// // let res = a + b;
+
+// let res = 500 + 500;
+// console.log(res);
+
+
+
+// -	Subtraction
+
+// let a = 500;
+// let b = 500;
+
+// let res = a - b;
+
+// console.log(res);
+
+
+
+// *	Multiplication
+
+// let a = 15;
+// let b = 15;
+
+// let res = a * b;
+// console.log(res);
+
+
+// /	Division
+// let a = 154;
+// let b = 24154;
+
+// let res = a / b;
+
+// console.log(res);
+
+
+
+
+
+//  %	Modulus (Remainder)
+
+// let a = 135;
+// let b = 15245;
+
+
+// let res = a % b;
+
+// console.log(res)
+
+
+
+
+// **	Exponentiation (ES2016)
+
+// let a = 10;
+// let b = 4;
+
+// let res = a ** b;   
+
+// console.log(res);
+
+
+
+
+
+// ++	Increment
+// --	Decrement
+
+
+
+// ++	Increment
+
+// let a = 5;
+
+// a++;      //  a = a + 1
+
+// console.log(a);
+
+
+
+// --	Decrement
+
+// let a = 5;
+
+// a--;   // a = a - 1
+
+// console.log(a);
